67a90747705bdfb8124f91329fee2d8fa3f1c263
Leandro Torres <leandroftm@gmail.com>
2026-03-23 20:08:56 -0300

    first commit

---
+++ src/main/java/com/leandroftm/taskmanager/taskmanagerv2/Taskmanagerv2Application.java
@@ -0,0 +1,13 @@
+package com.leandroftm.taskmanager.taskmanagerv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taskmanagerv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Taskmanagerv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/leandroftm/taskmanager/taskmanagerv2/Taskmanagerv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leandroftm.taskmanager.taskmanagerv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taskmanagerv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
