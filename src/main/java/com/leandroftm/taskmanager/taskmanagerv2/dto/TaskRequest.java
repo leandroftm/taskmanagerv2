41dd3a8c4c300c52fe7019985e91d16db29c09e6
Leandro Torres <leandroftm@gmail.com>
2026-03-23 21:10:56 -0300

    feat: dto created

--- src/main/java/com/leandroftm/taskmanager/taskmanagerv2/dto/TaskRequest.java
+++ src/main/java/com/leandroftm/taskmanager/taskmanagerv2/dto/TaskRequest.java
@@ -1,4 +1,26 @@
 package com.leandroftm.taskmanager.taskmanagerv2.dto;
 
-public record TaskRequest() {
+import com.leandroftm.taskmanager.taskmanagerv2.domain.enums.TaskPriority;
+import com.leandroftm.taskmanager.taskmanagerv2.domain.enums.TaskStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDateTime;
+
+public record TaskRequest(
+        @NotNull
+        @Size(min = 1, max = 50)
+        String title,
+        @Size(max = 255)
+        String description,
+        @NotBlank
+        @Size(min = 1, max = 25)
+        TaskPriority taskPriority,
+        @NotBlank
+        @Size(min = 1, max = 25)
+        TaskStatus taskStatus,
+        @NotNull
+        LocalDateTime dueDate
+) {
 }
--- src/main/java/com/leandroftm/taskmanager/taskmanagerv2/dto/TaskResponse.java
+++ src/main/java/com/leandroftm/taskmanager/taskmanagerv2/dto/TaskResponse.java
@@ -1,4 +1,18 @@
 package com.leandroftm.taskmanager.taskmanagerv2.dto;
 
-public record TaskResponse() {
+import com.leandroftm.taskmanager.taskmanagerv2.domain.enums.TaskPriority;
+import com.leandroftm.taskmanager.taskmanagerv2.domain.enums.TaskStatus;
+
+import java.time.LocalDateTime;
+
+public record TaskResponse(
+        Long id,
+        String title,
+        String description,
+        TaskStatus taskStatus,
+        TaskPriority taskPriority,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt,
+        LocalDateTime dueDate
+) {
 }
