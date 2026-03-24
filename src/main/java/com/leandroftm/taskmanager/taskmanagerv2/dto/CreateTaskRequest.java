ddfbdf1e7fb7a331757a072c3271139f0d2b047f
Leandro Torres <leandroftm@gmail.com>
2026-03-23 22:48:04 -0300

    refactor: split task request into create and update

--- src/main/java/com/leandroftm/taskmanager/taskmanagerv2/dto/CreateTaskRequest.java
+++ src/main/java/com/leandroftm/taskmanager/taskmanagerv2/dto/CreateTaskRequest.java
@@ -1,26 +1,23 @@
 package com.leandroftm.taskmanager.taskmanagerv2.dto;
 
 import com.leandroftm.taskmanager.taskmanagerv2.domain.enums.TaskPriority;
-import com.leandroftm.taskmanager.taskmanagerv2.domain.enums.TaskStatus;
+import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDateTime;
 
-public record TaskRequest(
+public record CreateTaskRequest(
         @NotBlank
         @Size(min = 1, max = 50)
         String title,
         @Size(max = 255)
         String description,
         @NotNull
-        @Size(min = 1, max = 25)
         TaskPriority taskPriority,
         @NotNull
-        @Size(min = 1, max = 25)
-        TaskStatus taskStatus,
-        @NotNull
+        @Future
         LocalDateTime dueDate
 ) {
 }
