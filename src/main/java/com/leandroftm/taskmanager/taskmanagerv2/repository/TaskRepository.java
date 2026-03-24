9ed9c3bc5af23ba3ff73009d483b2567c968d69c
Leandro Torres <leandroftm@gmail.com>
2026-03-23 22:50:54 -0300

    feat: add 'not id' filter

--- src/main/java/com/leandroftm/taskmanager/taskmanagerv2/repository/TaskRepository.java
+++ src/main/java/com/leandroftm/taskmanager/taskmanagerv2/repository/TaskRepository.java
@@ -1,7 +1,12 @@
 package com.leandroftm.taskmanager.taskmanagerv2.repository;
 
 import com.leandroftm.taskmanager.taskmanagerv2.domain.entity.Task;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    boolean existsByTitleIgnoreCase(String title);
+
+    boolean existsByTitleIgnoreCaseAndIdNot(String title, Long id);
 }
