b4cd3274436a04c0761c59bacaa581c42ea6a829
Leandro Torres <leandroftm@gmail.com>
2026-04-01 01:30:06 -0300

    feat: added spring security

--- src/main/java/com/leandroftm/taskmanager/taskmanagerv2/SecurityConfig.java
+++ src/main/java/com/leandroftm/taskmanager/taskmanagerv2/SecurityConfig.java
@@ -1,9 +1,10 @@
-package config;
+package com.leandroftm.taskmanager.taskmanagerv2;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,8 +13,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/h2.console/**").permitAll()
                         .anyRequest().permitAll()
                 );
         return http.build();
